2e3fffac44d99b46e6157b8ad914b668e646c707
Fojan Babaali <fojan.Babaali@carmanah.com>
2023-04-10 16:34:10 -0700

    update

---
+++ src/Fojan.js
@@ -0,0 +1,4 @@
+const Fojan = () => {
+  return <div>My name is Fojan</div>;
+};
+export default Fojan;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// 1) Import the React and ReactDOM libraries
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// 2) Get a refrence to the div with ID root
+const el = document.getElementById("root");
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// 3) Tell react to take control of that element
+const root = ReactDOM.createRoot(el);
+
+// 4) Create a component: component is a Js function that returns JSX
+const App = () => {
+  // Computations
+  const message = () => {
+    let msg = "Hello";
+    if(Math.random() < 0.5){
+      msg = "Bye";
+    }
+    return msg;
+  };
+  const time = () =>{
+    return (new Date().toLocaleTimeString());
+  }
+  const identity = () => {
+    const name = "Fojan";
+    const age = "33";
+    return `My name is ${name} and I am ${age} years old.`
+  }
+// Show result on screen
+const myArray = [1,2,3,4];
+  return (
+  <div>
+    <h3>{message()}</h3>
+    <div>{time()}</div>
+    <p>{identity()}</p>
+    <input type="number" min={5} max={20} style={{width:"10%"}} placeholder="i.e. 10"/>
+    <div>{myArray}</div>
+  </div>
+  );
+};
+
+root.render(<App />);
