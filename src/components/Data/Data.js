6e75b09658eed230cb45eb13ac4de7f5f61c7932
Fojan Babaali <fojan.Babaali@carmanah.com>
2023-04-12 12:14:11 -0700

    Done

--- src/components/Data/Data.js
+++ src/components/Data/Data.js
@@ -24,12 +24,8 @@ const data = [
       "https://user-images.githubusercontent.com/23070621/231288969-f202c472-8e73-4298-be52-22ec8dc53611.png",
     alt: "Alexa Logo",
     description: `There are many variations of passages of Lorem Ipsum available, 
-    but the majority have suffered alteration in some form, by injected humour,
-    or randomised words which don't look even slightly believable. 
-    If you are going to use a passage of Lorem Ipsum, you need to be
-    sure there isn't anything embarrassing hidden in the middle of text. 
-    All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary,
-    making this the first true generator on the Internet. It uses a dictionary of over 200 Latin 
+    but the majority have suffered alteration in some form, by injected humour,. 
+    All the Lhe Internet. It uses a dictionary of over 200 Latin 
     words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks
     reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour,
     or non-characteristic words etc.
@@ -44,13 +40,6 @@ const data = [
     description: `There are many variations of passages of Lorem Ipsum available, 
     but the majority have suffered alteration in some form, by injected humour,
     or randomised words which don't look even slightly believable. 
-    If you are going to use a passage of Lorem Ipsum, you need to be
-    sure there isn't anything embarrassing hidden in the middle of text. 
-    All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary,
-    making this the first true generator on the Internet. It uses a dictionary of over 200 Latin 
-    words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks
-    reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour,
-    or non-characteristic words etc.
     `,
   },
 ];
